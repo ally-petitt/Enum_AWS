3f7428754ad65a20ee4249a4c8124bd0551b731d
ally-petitt <allypetitt@gmail.com>
2023-05-07 11:04:07 -0700

    add more user options

--- modules/enum_s3.py
+++ modules/enum_s3.py
@@ -20,27 +20,37 @@ class EnumS3:
 
     Methods
     -------
+    run_all_enum_checks()
+        Runs all enumeration checks so that data can be collected as an attribute
     lookup_domain()
         Look up the IP address associated with the domain name and do a reverse 
         DNS lookup to populate the attributes domain_ip and reverse_domain_name
+
+
     check_bucket()
         Checks if the reverse_domain_name string is consistent with the naming
         conventions of an S3 bucket
-    run_all_enum_checks()
-        Runs all enumeration checks so that data can be collected as an attribute
+    check_bucket_listing()
+        Check if permissions allow listing of bucket permissions
+    check_bucket_upload()
+        Check if bucket permissions allow for downloading files
+    check_bucket_download()
+        Check if bucket permissions allow for downloading files
+    enum_bucket_permissions()
+        Check for directory listing, download, and download permissions
 
     '''
 
 
-    def __init__(self, domain: str) -> None:
+    def __init__(self, options: dict) -> None:
         """
         Parameters
         ----------
-        domain : str
-            The domain name of the target to enumeration (required)
+        options : dict
+            The configuration options for enumerating S3 buckets
         """
 
-        self.domain = domain
+        self.options = options
 
     
     def run_all_enum_checks(self) -> None:
@@ -48,9 +58,10 @@ class EnumS3:
         Runs all enumeration checks so that data can be collected as an attribute
         """
 
-
         self.lookup_domain()
         self.check_bucket()
+
+        if self.isBucket: self.enum_bucket_permissions()
 
 
     def lookup_domain(self) -> None:
@@ -61,7 +72,7 @@ class EnumS3:
 
         import socket
 
-        self.domain_ip = socket.gethostbyname(self.domain)
+        self.domain_ip = socket.gethostbyname(self.options["domain"])
         logging.info(f"Domain IP address is {self.domain_ip}")
 
         self.reverse_domain_name = socket.gethostbyaddr(self.domain_ip)[0]
@@ -83,4 +94,39 @@ class EnumS3:
         if self.isBucket: logging.info("Domain is an S3 bucket")
         else: logging.warning("Domain is NOT an S3 bucket")
 
+
+    def enum_bucket_permissions(self) -> None:
+        """ 
+        Checks for bucket listing, download, and upload permissions
+        if permitted in user settings
+        """
+
+        if self.options["list_s3_bucket"]: self.check_bucket_listing()
+        if self.options["check_s3_upload"]: logging.debug("check upload")
+        if self.options["check_s3_download"]: logging.debug("check download")
+        
+
+    
+    def check_bucket_listing(self) -> None:
+        """ 
+        Check if permissions allow listing of bucket permissions
+        """
+
+        pass
+
+
+    def check_bucket_download(self) -> None:
+        """ 
+        Check if bucket permissions allow for downloading files
+        """
+
+        pass
+
+
+    def check_bucket_upload(self) -> None:
+        """ 
+        Check if bucket permissions allow for uploading files
+        """
+
+        pass
 
